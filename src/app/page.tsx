5c97143592eac54f70797814a1b6dc1cd73fbda6
tecladochen <tecladochen@gmail.com>
2024-11-03 15:01:43 +0800

    tailwind 初体验

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div><Button>Click Me</Button></div>
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl font-bold">Create App</h1>
+        <Input name="name" placeholder="App Name"></Input>
+        <Textarea name="description" placeholder="Description"></Textarea>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
   );
 }
